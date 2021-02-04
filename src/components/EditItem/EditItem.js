d15213eb14bf7df3cd6cd242d3affeb975ac3ccc
krisztiansark <sarkozikrisztian94@gmail.com>
2021-02-04 18:48:05 +0100

    removed comment & conditional

--- src/components/EditItem/EditItem.js
+++ src/components/EditItem/EditItem.js
@@ -88,109 +88,107 @@ function EditItem(props) {
         color={COLORS.background}
         open={isLoading || isUsersLoading || isPatchLoading}
       />
-      {!isLoading && (
-        <Container
-          data-testid="container"
-          open={isUsersLoading || isPatchLoading}
-        >
-          <Row>
-            <Col col="12" md="6">
-              <H1 main>Edit Item</H1>
-              <Row mb="5" mt="4">
-                <Button special onClick={handleHome}>
-                  Go to shopping list
-                </Button>
-              </Row>
-              <Form onSubmit={handleSubmit}>
-                <Row w="100" mt="2" mb="3">
-                  <Col col="12">
-                    <Label mt="3" htmlFor="select">
-                      Choose the assignee:
-                    </Label>
-                    <Col mt="1" col="12" md="6">
-                      <Select
-                        name="select"
-                        value={info.assignedTo.id}
-                        onChange={handlePerson}
-                      >
-                        {users.map((user, i) => (
-                          <option key={user.id} value={user.id}>
-                            {user.name}
-                          </option>
-                        ))}
-                      </Select>
-                    </Col>
-                    <Col mt="2" col="6">
-                      <Img
-                        sm
-                        alt={info.assignedTo.name}
-                        src={info.assignedTo.profilePictureUrl}
-                      />
-                    </Col>
-                  </Col>
-                </Row>
-                <Label htmlFor="name">Edit item name:</Label>
-                <Row>
-                  <Col mb="4">
-                    <Input
-                      type="text"
-                      name="name"
-                      value={info.name}
-                      onChange={handleChange}
-                    />
-                  </Col>
-                </Row>
-                <Label htmlFor="description">Edit item description:</Label>
-                <Row mb="3" mt="2">
-                  <Col col="11" md="10">
-                    <Textarea
-                      type="text"
-                      name="description"
-                      onChange={handleChange}
-                      value={info.description}
-                    />
-                  </Col>
-                  <P>Used {info.description.length} characters out of 150.</P>
-                </Row>
 
-                <Label htmlFor="date">Edit due date:</Label>
-                <DatePickerStyled
-                  name="date"
-                  selected={startDate}
-                  onChange={handleChangeDate}
-                  dateFormat="yyyy-MM-dd"
-                />
-                <Col mt="4" col="10">
-                  <H3>
-                    Modifications won't be saved until you press the submit
-                    button.
-                  </H3>
+      <Container
+        data-testid="container"
+        open={isUsersLoading || isPatchLoading || isLoading}
+      >
+        <Row>
+          <Col col="12" md="6">
+            <H1 main>Edit Item</H1>
+            <Row mb="5" mt="4">
+              <Button special onClick={handleHome}>
+                Go to shopping list
+              </Button>
+            </Row>
+            <Form onSubmit={handleSubmit}>
+              <Row w="100" mt="2" mb="3">
+                <Col col="12">
+                  <Label mt="3" htmlFor="select">
+                    Choose the assignee:
+                  </Label>
+                  <Col mt="1" col="12" md="6">
+                    <Select
+                      name="select"
+                      value={info.assignedTo.id}
+                      onChange={handlePerson}
+                    >
+                      {users.map((user, i) => (
+                        <option key={user.id} value={user.id}>
+                          {user.name}
+                        </option>
+                      ))}
+                    </Select>
+                  </Col>
+                  <Col mt="2" col="6">
+                    <Img
+                      sm
+                      alt={info.assignedTo.name}
+                      src={info.assignedTo.profilePictureUrl}
+                    />
+                  </Col>
                 </Col>
+              </Row>
+              <Label htmlFor="name">Edit item name:</Label>
+              <Row>
+                <Col mb="4">
+                  <Input
+                    type="text"
+                    name="name"
+                    value={info.name}
+                    onChange={handleChange}
+                  />
+                </Col>
+              </Row>
+              <Label htmlFor="description">Edit item description:</Label>
+              <Row mb="3" mt="2">
+                <Col col="11" md="10">
+                  <Textarea
+                    type="text"
+                    name="description"
+                    onChange={handleChange}
+                    value={info.description}
+                  />
+                </Col>
+                <P>Used {info.description.length} characters out of 150.</P>
+              </Row>
 
-                <Row
-                  w="75"
-                  styled={{
-                    borderTop: `3px solid ${COLORS.border}`,
-                  }}
-                  mb="2"
-                  mt="4"
-                >
-                  <Row mt="3" mb="2">
-                    <Col col="6" md="4">
-                      <Button onClick={handleBack}>Back</Button>
-                    </Col>
-                    <Col col="6" md="4">
-                      <Button type="submit">Submit</Button>
-                    </Col>
-                  </Row>
+              <Label htmlFor="date">Edit due date:</Label>
+              <DatePickerStyled
+                name="date"
+                selected={startDate}
+                onChange={handleChangeDate}
+                dateFormat="yyyy-MM-dd"
+              />
+              <Col mt="4" col="10">
+                <H3>
+                  Modifications won't be saved until you press the submit
+                  button.
+                </H3>
+              </Col>
+
+              <Row
+                w="75"
+                styled={{
+                  borderTop: `3px solid ${COLORS.border}`,
+                }}
+                mb="2"
+                mt="4"
+              >
+                <Row mt="3" mb="2">
+                  <Col col="6" md="4">
+                    <Button onClick={handleBack}>Back</Button>
+                  </Col>
+                  <Col col="6" md="4">
+                    <Button type="submit">Submit</Button>
+                  </Col>
                 </Row>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      )}
+              </Row>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </>
-    // <> </>
   );
 }
 
